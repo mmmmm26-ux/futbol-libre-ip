3f696d5115e68d246cdd3be32ca1949d5bd895da
mmmmm26-ux <av4281219@gmail.com>
2025-06-29 13:08:58 -0500

    Update player.js

--- public/player.js
+++ public/player.js
@@ -1,1 +1,12 @@
-
+function playChannel(name, url) {
+  const video = document.getElementById(player-${name});
+  if (Hls.isSupported()) {
+    const hls = new Hls();
+    hls.loadSource(url);
+    hls.attachMedia(video);
+    hls.on(Hls.Events.MANIFEST_PARSED, () => video.play());
+  } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+    video.src = url;
+    video.addEventListener('loadedmetadata', () => video.play());
+  }
+}
