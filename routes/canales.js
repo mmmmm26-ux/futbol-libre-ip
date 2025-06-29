d688ce950e79d36da6d3d7f3749c8cbc24987df1
mmmmm26-ux <av4281219@gmail.com>
2025-06-29 13:08:02 -0500

    Update canales.js

--- routes/canales.js
+++ routes/canales.js
@@ -1,1 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const canales = require('../data/canales.json');
 
+router.get('/', (req, res) => {
+  res.render('index', { canales });
+});
+
+module.exports = router;
