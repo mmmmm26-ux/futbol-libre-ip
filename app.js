048e8715aa0578a974ee12133f4fdcc0fbb0104d
mmmmm26-ux <av4281219@gmail.com>
2025-06-29 13:20:44 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 const canalesRouter = require('./routes/canales');
+const proxyRoutes = require('./routes/proxy');
+app.use('/proxy', proxyRoutes);
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
